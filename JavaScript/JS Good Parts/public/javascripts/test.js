ab3bc527552130a11c5a842f877669749e8ee637
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2018-08-08 09:50:01 +0900

    180808 TIL JS Good Parts Chapter 4

--- JavaScript/JS Good Parts/public/javascripts/test.js
+++ JavaScript/JS Good Parts/public/javascripts/test.js
@@ -1,29 +1,27 @@
-var stooge = {
-    "first-name": "Moore",
-    "last-name": "Roger"
+var myObject = {
+    value: 0,
+    increment: function(inc) {
+        this.value += typeof inc === 'number' ? inc : 1;
+    },
+    getValue: function () {
+        return this.value;
+    }
 };
 
-stooge.nickname = "The Freak";
+myObject.increment();
+console.log(myObject.value);        // 1
+myObject.increment(2);
+console.log(myObject.value);        // 3
 
-if(typeof Object.create !== 'function') {
-    Object.create = function(o) {
+myObject.double = function() {
 
-        var F = function() { };
-        F.prototype = o;
-        return new F();
+    var that = this;
+    var helper = function() {
+        that.value *= 2;
     };
-}
 
-var another_stooge = Object.create(stooge);
+    helper();
+};
 
-/** 프로토타입 연결은 값의 갱신에 영향을 받지 않는다. 즉, 객체를 변경하더라도 객체의 프로토타입에는 영향을 미치지 않는다. */
-another_stooge["first-name"] = "Nick";
-another_stooge["last-name"] = "Make";
-another_stooge.nickname = "The Horror";
-
-/** 객체에서 특정 속성을 삭제했는데 같은 이름의 속성이 프로토타입 체인에 있다면, 그 프로토타입의 속성이 나타난다. */
-console.log(another_stooge.nickname);
-
-delete another_stooge.nickname;
-
-console.log(another_stooge.nickname);
+myObject.double();
+console.log(myObject.getValue());
