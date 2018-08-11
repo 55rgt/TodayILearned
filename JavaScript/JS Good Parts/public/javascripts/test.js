9362a1ef7db32501a8c10f7400c2df507a0b1cc5
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2018-08-11 22:55:33 +0900

    180811 TIL

--- JavaScript/JS Good Parts/public/javascripts/test.js
+++ JavaScript/JS Good Parts/public/javascripts/test.js
@@ -1,20 +1,15 @@
-var foo = function() {
-    var a = 3, b = 5;
+function scopeExam(){
+    scope = 20;
+    console.log("scope = " +scope);
+}
 
-    console.log(a,b);       // 3, 5
-
-    var bar = function() {
-        var b = 7, c = 11;
-
-        console.log(a,b,c); // 3, 7, 11
-
-        a += b + c;
-        console.log(a,b,c); // 21, 7, 11
-    };
-
-    bar();
-    console.log(a,b);   // 21, 5
-
-};
-
-foo();
+function scopeExam2(){
+    console.log("scope = " + scope);
+}
+scopeExam();
+scopeExam2();
+//실행결과
+/*
+scope=20
+scope=20
+*/
