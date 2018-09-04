efd883b4a22579a180e44d800acba710b9c58a62
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2018-09-04 22:17:59 +0900

    180904 TIL JavaScript JS Good Parts Chapter 6

--- JavaScript/JS Good Parts/public/javascripts/test.js
+++ JavaScript/JS Good Parts/public/javascripts/test.js
@@ -1,40 +1,4 @@
-var eventuality = function (that) {
+let numbers = [1,2,3,4,5,6];
+numbers.a = 66;
 
-    var registry = {};
-
-    that.fire = function (event) {
-
-        var array,
-            func,
-            handler,
-            i,
-            type = typeof event === 'string' ? event : event.type;
-
-        if (registry.hasOwnProperty(type)) {
-            array = registry[type];
-            for (i = 0; i < array.length; i++) {
-                handler = array[i];
-                func = handler.method;
-                if (typeof func === 'string') func = this[func];
-                func.apply(this, handler.parameters || [event]);
-            }
-        }
-        return this;
-    };
-
-    that.on = function (type, method, params) {
-
-        var handler = {
-            method: method,
-            params: params,
-        };
-
-        if (registry.hasOwnProperty(type)) {
-            registry[type].push(handler);
-        } else {
-            registry[type] = [handler];
-        }
-        return this;
-    };
-    return that;
-};
+console.log(numbers.length);
